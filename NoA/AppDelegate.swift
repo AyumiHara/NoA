888efa61c30f66766c59fb7d1826204c875f4071
AyumiHara <mapledog0425@gmail.com>
2018-05-04 23:13:59 +0900

    画面作り終わり 頑張るぞい

--- NoA/AppDelegate.swift
+++ NoA/AppDelegate.swift
@@ -13,6 +13,7 @@ import FireBase
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
